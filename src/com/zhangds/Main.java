da77159c73360434c4ef7761d572b449e37db20d
zhangds <1282906135@qq.com>
2024-07-14 23:31:51 +0800

    feat: jdk8源码本地环境编译

---
+++ src/com/zhangds/Main.java
@@ -0,0 +1,17 @@
+package com.zhangds;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        HashMap hashMap = new HashMap<>();
+        hashMap.put("k1","v1");
+        hashMap.put("k2","v2");
+        hashMap.put("k3","v3");
+        hashMap.put("k4","v4");
+        hashMap.put("k5","v5");
+        hashMap.get("k2");
+    }
+}
